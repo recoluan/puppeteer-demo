a6bf61cf28ac8686c43d5e0a2778437cd55a6c91
reco_luan <recoluan@outlook.com>
2019-10-11 20:15:30 +0800

    fix: style

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,19 @@
     },
     bindEvents () {
       this.$oGetInfo.on('click', this.getInfo.bind(this))
+      $('  #dropdownMenu1').dropdown('toggle')
     },
     getInfo () {
       recoFetch('/getInfo').then((res, req) => {
         const data = res.data
         let domStr = ''
         for(let i = 0, len = data.length; i < len; i++) {
-          domStr = domStr + `<li class="list-group-item">
-            <span>${data[i].title}</span>
-            <span>${data[i].date}</span>
-            <button class="btn btn-primary place-order" data-index="${i}">下单</bu>
-          </li>`
+          domStr = domStr + `<tr>
+            <td>${i}</td>
+            <td>${data[i].title}</td>
+            <td>${data[i].date}</td>
+            <td><button class="btn btn-primary place-order" data-index="${i}">下单</button></td>
+          </tr>`
         }
         this.$oOrderList.html(domStr)
         
@@ -39,7 +41,6 @@
         $oOrderList.find('li').eq(overIndex).css({
           background: '#f2f2f2'
         })
-        
       })
     }
   }
