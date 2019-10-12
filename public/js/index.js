65555f591ec5464e19d9449b4df1703a672e6d03
reco_luan <recoluan@outlook.com>
2019-10-12 15:22:26 +0800

    fix: gui

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,9 @@
   const HandleOrder = function () {
     this.$oGetInfo = $('.get-info')
     this.$oOrderList = $('.order-list')
+    this.$oHostBusiness = $('.host-business')
     this.$oSpan = null
+    this.id = ''
   }
 
   HandleOrder.prototype = {
@@ -11,37 +13,58 @@
     },
     bindEvents () {
       this.$oGetInfo.on('click', this.getInfo.bind(this))
-      $('  #dropdownMenu1').dropdown('toggle')
+      this.$oHostBusiness.on('click', this.getHostBusiness.bind(this))
     },
+    // 获取订单列表
     getInfo () {
-      recoFetch('/getInfo').then((res, req) => {
+      if (this.id == '') {
+        alert('请先选择主机厂商')
+        return
+      }
+      recoFetch('/getInfo').then(res => {
         const data = res.data
-        let domStr = ''
-        for(let i = 0, len = data.length; i < len; i++) {
-          domStr = domStr + `<tr>
-            <td>${i}</td>
-            <td>${data[i].title}</td>
-            <td>${data[i].date}</td>
-            <td><button class="btn btn-primary place-order" data-index="${i}">下单</button></td>
-          </tr>`
-        }
-        this.$oOrderList.html(domStr)
-        
-        const $oBtnPlaceOrder = this.$oOrderList.find('li .place-order')
-        $oBtnPlaceOrder.on('click', this.handleOrder)
+        this.initPlaceOrder(data)
       })
     },
+    // 初始化订单列表和绑定下单事件
+    initPlaceOrder (data) {
+      let domStr = ''
+      for(let i = 0, len = data.length; i < len; i++) {
+        domStr = domStr + `<tr>
+          <td>${i}</td>
+          <td>${data[i].title}</td>
+          <td>${data[i].date}</td>
+          <td><button class="btn btn-primary place-order" data-index="${i}">下单</button></td>
+        </tr>`
+      }
+      this.$oOrderList.html(domStr)
+
+      const $oBtnPlaceOrder = this.$oOrderList.find('tr .place-order')
+      $oBtnPlaceOrder.on('click', this.handleOrder)
+    },
+    // 下单
     handleOrder (e) {
       const index = e.target.dataset.index
       const $oOrderList = $('.order-list')
+      $oOrderList.find('tr').eq(index).find('place-order').text('进行中').css({
+        disabled: 'disabled'
+      })
       
       recoFetch(`/handleOrder/${index}`).then(res => {
         const overIndex = res.data.index
         alert(`订单${overIndex}：下单成功！`)
-        $oOrderList.find('li').eq(overIndex).css({
+        $oOrderList.find('tr').eq(overIndex).find('place-order').text('完成')
+        $oOrderList.find('tr').eq(overIndex).css({
           background: '#f2f2f2'
         })
       })
+    },
+    // 获取主机厂商
+    getHostBusiness (e) {
+      const id = e.target.dataset.id
+      const  text = e.target.outerText
+      $('.order-wrapper .screen-wrapper button').html(`${text}<span class="caret"></span>`)
+      this.id = id
     }
   }
 
